255110230b61534f9424ab29e200a1e8d588f94a
Erik Andersen <erik.andersen@parivedasolutions.com>
2017-12-16 04:15:19 -0600

    - removed obj files from git - added scripts for the gameplay - added scripts to allow people to replay - adjusted weight and drag on the golf ball

--- ar_putt_putt_2/Assets/Scripts/Ball.cs
+++ ar_putt_putt_2/Assets/Scripts/Ball.cs
@@ -2,108 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.EventSystems;
 
 public class Ball : MonoBehaviour {
 
 	Vector3 _initialPosition;
 	float _initialTime;
-	Rigidbody _rigidBody;
+
+	private Renderer Renderer;
+	public Rigidbody RigidBody;
 
 	#region Lifecycle
 
 	void Start () 
 	{
-		_rigidBody = GetComponent<Rigidbody>();
-	}
+		RigidBody = GetComponent<Rigidbody>();
+		Renderer = gameObject.GetComponentInChildren<MeshRenderer>();
 
-	void Update () 
-	{
-		
+		gameObject.SetActive(false);
 	}
 
 	#endregion
+
 
 	#region Public Methods
 
-	public void DragStart () 
+	public void Launch (Vector3 velocity)
 	{
-		Debug.Log("DRAG START CALLED");
+		gameObject.SetActive(true);
 
-		_initialPosition = Input.mousePosition;
-		_initialTime = Time.time;
-
-		Debug.Log("DRAG START POSITION: " + _initialPosition);
+		RigidBody.velocity = velocity;
 	}
 
-	public void DragEnd () 
+	public void Reset ()
 	{
-		Debug.Log("DRAG END CALLED");
+		RigidBody.position = new Vector3(x:0f, y:0.2f, z:0f);
+		RigidBody.velocity = new Vector3(x:0f, y:0.0f, z:0f);
 
-		var currentPosition = Input.mousePosition;
-		var currentTime = Time.time;
-		var duration = currentTime - _initialTime;
-
-		var speedX = (currentPosition.x - _initialPosition.x)/duration;
-		var speedZ = (currentPosition.y - _initialPosition.y)/duration;
-
-		var velocity = new Vector3(speedX, 0, speedZ);
-
-		Debug.Log("DRAG END POSITION: " + currentPosition);
-		Debug.Log("DRAG END VELOCITY: " + velocity);
-
-		Launch(velocity);
-	}
-
-	#endregion
-
-	#region Private Methods
-
-	private void Launch (Vector3 velocity)
-	{
-		_rigidBody.useGravity = true;
-
-		_rigidBody.velocity = velocity;
-//		_audioSource.Play ();
-//		_isLaunched = true;
+		gameObject.SetActive(true);
 	}
 
 	#endregion
 }
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using System;
-//
-//[RequireComponent(typeof(BowlingBall))]
-//public class DragLaunch : MonoBehaviour {
-//
-//	// Variables
-//	BowlingBall _ball;
-//	Vector3 _initialPosition;
-//	float _initialTime;
-//
-//
-//	// Use this for initialization
-//	void Start () {
-//		_ball = gameObject.GetComponent<BowlingBall>();
-//	}
-//
-//	public void DragStart() {
-//		_initialPosition = Input.mousePosition;
-//		_initialTime = Time.time;
-//	}
-//
-//	public void DragEnd() {
-//		var currentPosition = Input.mousePosition;
-//		var currentTime = Time.time;
-//		var duration = currentTime - _initialTime;
-//
-//		var speedX = (currentPosition.x - _initialPosition.x)/duration;
-//		var speedZ = (currentPosition.y - _initialPosition.y)/duration;
-//
-//		var velocity = new Vector3(speedX, 0, speedZ);
-//
-//		_ball.Launch(velocity);
-//	}
-//}
---
+++ ar_putt_putt_2/Assets/Scripts/Drag.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Ball))]
+public class Drag : MonoBehaviour {
+
+	Vector3 _initialPosition;
+	float _initialTime;
+	float _dampener = 0.05f;
+	Ball _ball;
+
+	#region Lifecycle
+
+	void Start () 
+	{
+		_ball = gameObject.GetComponent<Ball>();
+	}
+
+	void Update () 
+	{
+		var currentVerticalPosition = _ball.RigidBody.position.y;
+
+		Debug.Log("VERTICAL POSITION: " + currentVerticalPosition);
+
+		if (currentVerticalPosition < 0.1f) 
+		{
+			_ball.RigidBody.position = new Vector3(x: _ball.RigidBody.position.x,
+												   y: 0.1f,
+												   z: _ball.RigidBody.position.z);
+		}
+	}
+
+	#endregion
+
+	#region Public Methods
+
+	public void DragStart () 
+	{
+		_initialPosition = Input.mousePosition;
+		_initialTime = Time.time;
+	}
+
+	public void DragEnd () 
+	{
+		Debug.Log("DRAG END CALLED");
+
+		var currentPosition = Input.mousePosition;
+		var currentTime = Time.time;
+		var duration = currentTime - _initialTime;
+
+		var speedX = _dampener * (currentPosition.x - _initialPosition.x)/duration;
+		var speedZ = _dampener * (currentPosition.y - _initialPosition.y)/duration;
+
+		var velocity = new Vector3(speedX, 0, speedZ);
+
+		_ball.Launch(velocity);
+	}
+
+	#endregion
+}
