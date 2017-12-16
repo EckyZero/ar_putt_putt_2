db841b9c1bb59da06704a8829292bac3bf40671b
Erik Andersen <erik.andersen@parivedasolutions.com>
2017-12-16 02:23:39 -0600

    - Fixed issue where ball was falling through the ground

--- ar_putt_putt_2/Assets/Scripts/Ball.cs
+++ ar_putt_putt_2/Assets/Scripts/Ball.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Ball : MonoBehaviour {
 
@@ -15,18 +16,29 @@ public class Ball : MonoBehaviour {
 		_rigidBody = GetComponent<Rigidbody>();
 	}
 
+	void Update () 
+	{
+		
+	}
+
 	#endregion
 
 	#region Public Methods
 
 	public void DragStart () 
 	{
+		Debug.Log("DRAG START CALLED");
+
 		_initialPosition = Input.mousePosition;
 		_initialTime = Time.time;
+
+		Debug.Log("DRAG START POSITION: " + _initialPosition);
 	}
 
 	public void DragEnd () 
 	{
+		Debug.Log("DRAG END CALLED");
+
 		var currentPosition = Input.mousePosition;
 		var currentTime = Time.time;
 		var duration = currentTime - _initialTime;
@@ -35,6 +47,9 @@ public class Ball : MonoBehaviour {
 		var speedZ = (currentPosition.y - _initialPosition.y)/duration;
 
 		var velocity = new Vector3(speedX, 0, speedZ);
+
+		Debug.Log("DRAG END POSITION: " + currentPosition);
+		Debug.Log("DRAG END VELOCITY: " + velocity);
 
 		Launch(velocity);
 	}
